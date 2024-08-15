8d7ee8e8bac2fb69d055d15e332586a3d28ddb6d
heisetiantang <92718554+heisetiantang@users.noreply.github.com>
2024-08-15 15:57:01 +0800

    编写连接状态检测

--- src/start_car.cpp
+++ src/start_car.cpp
@@ -1,9 +1,12 @@
 #include <ros/ros.h>
 #include <std_msgs/String.h>
 #include <std_msgs/Int8.h>
+#include <std_msgs/Int64.h>
+#include <std_msgs/Bool.h>
 #include <sys/utsname.h>
 
 using namespace std;
+std_msgs::Int64 is_connected_msg;
 // 定义文件路径
 // string file_start_a = "/swarm_ros_bridge/src/swarm_ros_bridge/shell/one-click_a.sh";
 // string file_start_b = "/swarm_ros_bridge/src/swarm_ros_bridge/shell/one-click_b.sh";
@@ -94,19 +97,30 @@ int main(int argc, char **argv)
     ros::NodeHandle nh;
     //   识别中文
     setlocale(LC_ALL, "");
+    is_connected_msg.data = 0;
+
     // 接收特定启动指令，并运行制定位置的命令shell文件，或者启动指定的指令
     ROS_INFO("终端指令等待");
     ros::Subscriber CMD_A = nh.subscribe<std_msgs::Int8>("cmd_A", 1, Callback_A);
     ros::Subscriber CMD_B = nh.subscribe<std_msgs::Int8>("cmd_B", 1, Callback_B);
     ros::Subscriber CMD_C = nh.subscribe<std_msgs::Int8>("cmd_C", 1, Callback_C);
+
+    // 持续发布是否连接信息
+    ros::Publisher is_connected = nh.advertise<std_msgs::Int64>("is_connected", 1);
+
     // 如果没有发布者，等待5s
-    if(!CMD_A.getNumPublishers()||!CMD_B.getNumPublishers()||!CMD_C.getNumPublishers())
+    if (!CMD_A.getNumPublishers() || !CMD_B.getNumPublishers() || !CMD_C.getNumPublishers())
     {
         ROS_INFO("等待发布者");
     }
+    ros::Rate loop_rate(10); // 10hz
     while (ros::ok())
     {
+        is_connected.publish(is_connected_msg);
+        is_connected_msg.data++;
+
         ros::spinOnce();
+        loop_rate.sleep();
     }
 
     return 0;
